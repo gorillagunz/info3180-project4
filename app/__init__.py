7eb2d1bf05a0551348704ea26cabee4c5e919f4b
Dane Miller <dmiller2310@hotmail.com>
2016-04-30 04:20:37 +0000

    Heroku setup

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,9 @@ db = SQLAlchemy(app)
 
 from models import *
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dev:dev@localhost/wishlist'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ihztujtsyworlv:PamYRUGMSJkBmHAyVFXLadvhrd@ec2-107-21-229-87.compute-1.amazonaws.com:5432/d5u75njlgmd9q9'db.drop_all()
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dev:dev@localhost/wishlist'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nbtgpccughvyvg:B-Nfv9o5llQAyv_crasERkAYcB@ec2-174-129-18-170.compute-1.amazonaws.com:5432/d1jv4v3ejj80l4'
+
 #db.drop_all()
 db.create_all()
 
